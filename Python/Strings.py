ba40f75258563a41522ebe140c78ce7b7cdd633e
Manoj Mukkamala <manojmukkamala11@gmail.com>
2019-02-23 16:21:39 -0500

    Update Strings.py

--- Python/Strings.py
+++ Python/Strings.py
@@ -95,6 +95,15 @@ if __name__ == '__main__':
     print(result)
 
 
+# Designer Door Mat
+N, M = map(int,input().split()) # More than 6 lines of code will result in 0 score. Blank lines are not counted.
+for i in range(1,N,2): 
+    print ((i * ".|.").center(M, "-"))
+print ('WELCOME'.center(M, "-"))
+for i in range(N-2,-1,-2): 
+    print ((i * ".|.").center(M, "-"))
+    
+
 # String Formatting
 def print_formatted(number):
     # your code goes here
@@ -104,3 +113,44 @@ def print_formatted(number):
 if __name__ == '__main__':
     n = int(input())
     print_formatted(n)
+    
+    
+# Capitalize
+#!/bin/python3
+iport math
+import os
+import random
+import re
+import sys
+# Complete the solve function below.
+def solve(s):
+    for x in s.split():
+        s = s.replace(x, x.capitalize())
+    return (s)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    result = solve(s)
+    fptr.write(result + '\n')
+    fptr.close()
+    
+    
+# The Minion Game 
+####
+
+####
+
+
+# Merge the Tools
+def merge_the_tools(string, k):
+    # your code goes here
+    for i in range(0, len(string), k):
+        s = string[i:i+k]
+        uni = []
+        for y in s:
+            if y not in uni:
+                uni.append(y)
+        print (''.join(uni))
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
