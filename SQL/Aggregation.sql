edb5f62f13ca3151496416d00bda9a65521ec84c
Manoj Mukkamala <manojmukkamala11@gmail.com>
2019-02-24 00:03:21 -0500

    Update Aggregation.sql

--- SQL/Aggregation.sql
+++ SQL/Aggregation.sql
@@ -42,3 +42,61 @@ SELECT TOP 1 (MONTHS*SALARY) AS EARNINGS, COUNT(*)
 FROM EMPLOYEE
 GROUP BY MONTHS*SALARY
 ORDER BY COUNT(*) DESC;
+                                                   
+                                                   
+# Weather Observation Station 2                                                   
+SELECT CONCAT(LEFT(ROUND(SUM(LAT_N), 2), 8),' ', LEFT(ROUND(SUM(LONG_W), 2), 8))
+FROM STATION;
+
+                                                      
+# Weather Observation Station 13
+SELECT CAST(SUM(LAT_N) AS NUMERIC(10, 4))
+FROM STATION
+WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
+
+                                  
+# Weather Observation Station 14                                                 
+SELECT CAST(MAX(LAT_N) AS NUMERIC(10, 4))
+FROM STATION
+WHERE LAT_N < 137.2345;
+                                  
+
+# Weather Observation Station 15
+SELECT CAST(LONG_W AS NUMERIC(10, 4))
+FROM STATION
+WHERE LAT_N IN
+(SELECT MAX(LAT_N) 
+FROM STATION
+WHERE LAT_N < 137.2345);
+                              
+
+# Weather Observation Station 16                                                   
+SELECT CAST(MIN(LAT_N) AS NUMERIC(10, 4))
+FROM STATION
+WHERE LAT_N > 38.7780;
+                                  
+
+# Weather Observation Station 17
+SELECT CAST(LONG_W AS NUMERIC(10, 4))
+FROM STATION
+WHERE LAT_N IN
+(SELECT MIN(LAT_N)
+FROM STATION
+WHERE LAT_N > 38.7780);
+                              
+
+# Weather Observation Station 18                                                  
+SELECT CAST(ABS(MIN(LAT_N)-MAX(LAT_N)) + ABS(MIN(LONG_W) - MAX(LONG_W)) AS NUMERIC(10, 4))
+FROM STATION;
+                                                                                   
+
+# Weather Observation Station 19 
+SELECT CAST(SQRT(SQUARE(MIN(LAT_N) - MAX(LAT_N)) + SQUARE(MIN(LONG_W) - MAX(LONG_W))) AS NUMERIC(10, 4))
+FROM STATION;
+                                                                                                 
+
+# Weather Observation Station 20                                                  
+SELECT CAST(S.LAT_N AS NUMERIC(10, 4)) MEDIAN 
+FROM STATION S 
+WHERE (SELECT COUNT(Lat_N) FROM STATION WHERE Lat_N < S.LAT_N) = (SELECT COUNT(Lat_N) FROM STATION WHERE Lat_N > S.LAT_N);
+
